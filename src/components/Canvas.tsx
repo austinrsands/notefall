f904d2b51177b43deae88ddb3059c478fe48f9f1
Austin Sands <austinrsands@gmail.com>
2020-12-22 01:49:33 -0600

    improved resizing

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import Size from '../interfaces/Size';
 import StyleProps from '../interfaces/StyleProps';
 
 const DEVICE_PIXEL_RATIO = window.devicePixelRatio || 1;
+
+// Number of times to draw per frame
 const DEFAULT_TARGET_FRAMERATE = 60;
 
 interface Props {
@@ -22,6 +24,38 @@ const Canvas: React.FC<Props & StyleProps> = ({
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const sizeRef = useRef<Size | null>(null);
 
+  // Returns true if canvas should resize, false otherwise
+  const shouldResize = useCallback((canvas) => {
+    if (sizeRef.current === null) return true;
+    const { width, height } = canvas.getBoundingClientRect();
+
+    // Determine if canvas size has changed
+    const sizeHasChanged =
+      sizeRef.current.width !== width || sizeRef.current.height !== height;
+
+    return sizeHasChanged;
+  }, []);
+
+  // Resizes the canvas associated with the given context
+  const resize = useCallback(
+    (context: CanvasRenderingContext2D) => {
+      const { width, height } = context.canvas.getBoundingClientRect();
+      // Resize canvas
+      context.canvas.width = width * DEVICE_PIXEL_RATIO;
+      context.canvas.height = height * DEVICE_PIXEL_RATIO;
+
+      // Scale context
+      context.scale(DEVICE_PIXEL_RATIO, DEVICE_PIXEL_RATIO);
+
+      // Update size ref
+      sizeRef.current = { width, height };
+
+      // Run callback
+      if (onResize) onResize(sizeRef.current);
+    },
+    [onResize],
+  );
+
   // Updates canvas
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -33,38 +67,15 @@ const Canvas: React.FC<Props & StyleProps> = ({
 
     // Check if canvas and context are defined
     if (canvas && context) {
-      // Resizes canvas if necessary
-      const resizeCanvas = () => {
-        const { width, height } = canvas.getBoundingClientRect();
-        if (
-          sizeRef.current?.width !== width ||
-          sizeRef.current?.height !== height
-        ) {
-          // Resize canvas
-          canvas.width = width * DEVICE_PIXEL_RATIO;
-          canvas.height = height * DEVICE_PIXEL_RATIO;
-
-          // Scale context
-          context.scale(DEVICE_PIXEL_RATIO, DEVICE_PIXEL_RATIO);
-
-          // Update size ref
-          sizeRef.current = { width, height };
-
-          // Call resize prop
-          if (onResize) onResize(sizeRef.current);
-        }
-      };
-
       // Initial Resize
-      resizeCanvas();
+      if (shouldResize(canvas)) resize(context);
 
       // Call setup if defined
       if (onSetup) onSetup(context);
 
       // Animation loop
       const animate = () => {
-        // Resize canvas
-        resizeCanvas();
+        if (shouldResize(canvas)) resize(context);
 
         // Reanimate based on target framerate
         timeoutID = setTimeout(() => {
@@ -84,7 +95,7 @@ const Canvas: React.FC<Props & StyleProps> = ({
       if (requestID) cancelAnimationFrame(requestID);
       if (timeoutID) clearTimeout(timeoutID);
     };
-  }, [targetFramerate, onSetup, onDraw, onResize]);
+  }, [targetFramerate, onSetup, onDraw, onResize, resize, shouldResize]);
 
   return <canvas ref={canvasRef} {...rest} />;
 };
