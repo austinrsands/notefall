d073b9ccc6d53df8fa44424214d37b758971b8ea
Austin Sands <austinrsands@gmail.com>
2020-12-27 13:16:32 -0600

    moved theming folder

--- src/app/component.tsx
+++ src/app/component.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Box, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
 import { SnackbarProvider } from 'notistack';
-import { DARK_THEME } from '../theming/themes';
 import { GameContextProvider } from './game-context/context';
 import DeviceManager from './device-manager/component';
 import Sketch from './sketch/component';
 import TopBar from './top-bar/component';
+import { DARK_THEME } from './theming/themes';
 
 const useStyles = makeStyles({
   sketch: {
